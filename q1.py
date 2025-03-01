f8416ff16ce8b1ce8438708ba39ba8dcef251e68
MrunaliBhabale <mrunalibhabale2956@gmail.com>
2025-03-01 13:27:28 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import google.generativeai as genai
+Api_Key ="AIzaSyCQrpvKrSnHCwbIKhl2OncdWzrk3n_zYkQ"
+model=genai.GenerativeModel("gemini-1.5-flash")
+
+st.title("AI-Powered Resume Gnerator")
+
+name=st.text_input("Enter your name")
+skills=st.text_input("Enter your skills")
+experience=st.text_area("Enter your work experience")
+education=st.text_area("Enter your education")
+
+if st. button("Generate Resume"):
+    try:
+        prompt= f"Generate a resume for {name} with skills in {skills}, experience in work {experience} and education in {education}"
+        responce=model.generate_content(prompt)
+    except:
+      st.error("error is generating")
+    else:
+      st.write("resume is generated")
+
+
