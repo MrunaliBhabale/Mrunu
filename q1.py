d1182ca40e8bb9b9dc866b3e3810a2d21dfd5ee1
MrunaliBhabale <mrunalibhabale2956@gmail.com>
2025-03-04 13:55:26 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import google.generativeai as genai
-Api_Key ="AIzaSyCQrpvKrSnHCwbIKhl2OncdWzrk3n_zYkQ"
+api_Key ="AIzaSyCQrpvKrSnHCwbIKhl2OncdWzrk3n_zYkQ"
 model=genai.GenerativeModel("gemini-1.5-flash")
-
-st.title("AI-Powered Resume Gnerator")
+def app():
+ st.title("AI-Powered Resume Gnerator")
 
 name=st.text_input("Enter your name")
 skills=st.text_input("Enter your skills")
 experience=st.text_area("Enter your work experience")
 education=st.text_area("Enter your education")
+click=st.button("Generate Resume")
+if click:
+   genai.configure(api_key="AIzaSyCQrpvKrSnHCwbIKhl2OncdWzrk3n_zYkQ") 
+   model=genai.GenerativeModel("gemini-1.5-flash")
+prompt= f"Generate a resume for {name} with skills in {skills}, experience in work {experience} and education in {education}"
+responce=model.generate_content(prompt)
+st.markdown(responce)
 
-if st. button("Generate Resume"):
-    try:
-        prompt= f"Generate a resume for {name} with skills in {skills}, experience in work {experience} and education in {education}"
-        responce=model.generate_content(prompt)
-    except:
-      st.error("error is generating")
-    else:
-      st.write("resume is generated")
-        download_file=123
+
 
